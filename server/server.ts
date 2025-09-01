2fa65a104e7375b122941ddf99b81ce1165b75fc
rajakuma6an2022 <rajakuma6an93@gmail.com>
2025-09-01 19:23:44 +0530

    update

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["https://portfolio-kd31.vercel.app", "http://localhost:5173"]
+  origin: ["https://rajakumaran-dev.vercel.app/"]
 }));
 app.use(bodyParser.json());
 
