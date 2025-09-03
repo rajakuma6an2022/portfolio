5a53d550993b293cdcd5b3b2ced57fb530822f11
rajakuma6an2022 <rajakuma6an93@gmail.com>
2025-09-03 16:44:50 +0530

    update

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base : "/"
+// });
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base : "/"
-});
+  preview: {
+    // allow your Render domain
+    allowedHosts: ['portfolio-frontend-vu2z.onrender.com'],
+    port: Number(process.env.PORT) || 4173, // keep $PORT
+    host: true, // allow 0.0.0.0
+  },
+})
