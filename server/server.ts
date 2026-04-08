6326af2f698cb6f68164020270f89c971e974944
rajakuma6an2022 <rajakuma6an93@gmail.com>
2026-04-08 09:35:10 +0530

    frontend url changed

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
-  origin: ["https://rajakumaran-dev.vercel.app","http://localhost:5173"]
+  origin: ["https://portfolio-frontend-vu2z.onrender.com","http://localhost:5173"]
 }));
 app.use(bodyParser.json());
 
